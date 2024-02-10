2021813f0c418e882d3d5e4d96f6afd0cc8fd99e
eha49 <habron.rasheed7@gmail.com>
2024-02-10 14:06:04 +0530

    Basic Styling and some JS

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import { rollsArray, drinksArray } from "./data.js";
 
+const menuRolls = document.getElementById('menu-section-rolls');
+
+function renderMenu(arr, element) {
+    const htmlString = arr.map(item => {
+        return `
+        <article class='menu-section-item'>
+            <img src=${item.image} alt='A menu item' class='menu-item-img'>
+            <div>
+                <h2 class='menu-item-name'>${item.name}</h2>
+                <p class='menu-item-ingredients'>${item.ingredients.join(', ')}</p>
+                <p class='menu-item-price'>$${item.price}</p>
+            </div>
+            <button id='add-item-btn' class='hover'>+</button>
+        </article>`
+    }).join('');
+
+    element.innerHTML = htmlString;
+}
+
+renderMenu(rollsArray, menuRolls);
